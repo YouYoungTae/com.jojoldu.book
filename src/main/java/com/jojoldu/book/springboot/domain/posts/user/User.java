da8690d81f2adbe51fa8a56e9b9f0b70fac43914
YouYoungTae <ytreyt@naver.com>
2021-06-13 12:59:30 +0900

    work a

--- src/main/java/com/jojoldu/book/springboot/domain/posts/user/User.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/user/User.java
@@ -1,5 +1,32 @@
-package com.jojoldu.book.springboot.domain.posts;
+package com.jojoldu.book.springboot.domain.posts.user;
 
+import com.jojoldu.book.springboot.domain.posts.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class User extends BaseTimeEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
 }
